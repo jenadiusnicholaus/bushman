781b9ac21c7bc3775a5f47004e8202081b14ccbc
jenadiusnicholaus <ucode888@gmail.com>
2024-11-01 16:54:15 +0300

    changed the  SECRET_KEY

--- settings/settings.py
+++ settings/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ih0$nqx*z3sc^s0m*b87oljaxd2jt=oem$41!u2_v)f&0h_ry^"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
