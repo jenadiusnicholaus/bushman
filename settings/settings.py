000a1d0844c7eebbb5b03913db0cb53322e880ff
jenadiusnicholaus <ucode888@gmail.com>
2024-11-03 22:49:12 +0300

    add trophyapi.japango.co.tz to  allowed hosts

--- settings/settings.py
+++ settings/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.0.162", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.0.162", "localhost", "127.0.0.1", "trophyapi.japango.co.tz"]
 
 
 # Application definition
@@ -97,16 +97,16 @@ WSGI_APPLICATION = "settings.wsgi.application"
 #     }
 # }
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": config("DATABASE_NAME"),
-        "USER": config("DATABASE_USER"),
-        "PASSWORD": config("DATABASE_PASSWORD"),
-        "HOST": config("DATABASE_HOST"),
-        "PORT": config("DATABASE_PORT"),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": config("DATABASE_NAME"),
+#         "USER": config("DATABASE_USER"),
+#         "PASSWORD": config("DATABASE_PASSWORD"),
+#         "HOST": config("DATABASE_HOST"),
+#         "PORT": config("DATABASE_PORT"),
+#     }
+# }
 
 DATABASES = {
     "default": {
